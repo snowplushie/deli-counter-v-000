e591fb0b591ad5f3fbaad5fa967b140cbaf96b71
Paige Wiener <snowplushie@yahoo.com>
2019-03-01 05:31:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,7 @@ def line(deli)
   if deli.empty?
    "The line is currently empty."
  else current_line="The line is currently:"
-   deli.each_with_index(person, index)
+   deli.each_with_index(1) do |person, index|
+     
 end
 end
