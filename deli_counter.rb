dc18d5b0332487c3f3dfc24a5ed24b1ee037305b
Paige Wiener <snowplushie@yahoo.com>
2019-03-01 05:39:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(katz_deli)
  else 
    current_line="The line is currently:"
    katz_deli.each_with_index do |person, index|
-     current_line << "#{index+1}. #{person}"
+     current_line << "#{index+1}. #{person} "
    end
    puts current_line
  end
