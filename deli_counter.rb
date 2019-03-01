9b14bd7be09b552d00bee1193272729d7241dc3a
Paige Wiener <snowplushie@yahoo.com>
2019-03-01 05:46:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,4 +17,10 @@ def take_a_number(katz_deli, person)
   puts "Welcome, #{person}. You are number #{katz_deli.size} in line."
 end
 
-def now_serving
+def now_serving(katz_deli)
+  if katz_deli == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.size}."
+  end
+end
