1498649ff8ff297dbc5599c15a57b97a9e87f370
Paige Wiener <snowplushie@yahoo.com>
2019-03-01 05:46:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,6 +21,6 @@ def now_serving(katz_deli)
   if katz_deli == 0
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{katz_deli.size}."
+    puts "Currently serving #{katz_deli.shift}."
   end
 end
